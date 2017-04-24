438122ecfa05052e3468675b4059d7e6a5b381b2
UnfinishedBusiness <travis.gillin@gmail.com>
2017-04-24 12:15:14 -0400

    Started testing on machine and fixed a few bugs

--- src/com/nc/pilot/lib/ncCommands.java
+++ src/com/nc/pilot/lib/ncCommands.java
@@ -10,38 +10,38 @@ package com.nc.pilot.lib;
  * @author travis
  */
 public class ncCommands {
-    public static String GoToOrigin = "G0 X0 Y0\r\n";
+    public static String GoToOrigin = "G90 G0 X0 Y0\r\n";
     public static String FeedHold = "!\r\n";
     public static String CycleStart = "~\r\n";
     public static String QueFlush = "%\r\n";
     
     //Y Jogging
-    public static String StartJogYPlus = "G91 G0 Y5 G90\r\n";
-    public static String StopJogYPlus = "!%\r\n";
+    public static String StartJogYPlus = "G91 G0 Y100\r\n";
+    public static String StopJogYPlus = "!%\r\nG90\r\n";
     
-    public static String StartJogYMinus = "G91 G0 Y-5 G90\r\n";
-    public static String StopJogYMinus = "!%\r\n";
+    public static String StartJogYMinus = "G91 G0 Y-100\r\n";
+    public static String StopJogYMinus = "!%\r\nG90\r\n";
     
     //X Jogging
-    public static String StartJogXPlus = "G91 G0 X5 G90\r\n";
-    public static String StopJogXPlus = "!%\r\n";
+    public static String StartJogXPlus = "G91 G0 X100\r\n";
+    public static String StopJogXPlus = "!%\r\nG90\r\n";
     
-    public static String StartJogXMinus = "G91 G0 X-5 G90\r\n";
-    public static String StopJogXMinus = "!%\r\n";
+    public static String StartJogXMinus = "G91 G0 X-100\r\n";
+    public static String StopJogXMinus = "!%\r\nG90\r\n";
     
     //Z Jogging
-    public static String StartJogZPlus = "G91 G0 Z5 G90\r\n";
-    public static String StopJogZPlus = "!%\r\n";
+    public static String StartJogZPlus = "G91 G0 Z100\r\n";
+    public static String StopJogZPlus = "!%\r\nG90\r\n";
     
-    public static String StartJogZMinus = "G91 G0 Z-5 G90\r\n";
-    public static String StopJogZMinus = "!%\r\n";
+    public static String StartJogZMinus = "G91 G0 Z-100\r\n";
+    public static String StopJogZMinus = "!%\r\nG90\r\n";
     
     //Set origins
-    public static String SetXOrigin = "G28.3 X0\r\n";
-    public static String SetYOrigin = "G28.3 Y0\r\n";
-    public static String SetZOrigin = "G28.3 Z0\r\n";
+    public static String SetXOrigin = "G92 X=0\r\nG53\r\nG28.3 X=0.000\r\n";
+    public static String SetYOrigin = "G92 Y=0\r\nG53\r\nG28.3 Y=0.000\r\n";
+    public static String SetZOrigin = "G92 Z=0\r\nG53\r\nG28.3 Z=0.000\r\n";
     
     //M Codes
-    public static String TorchOn = "M3\r\n";
-    public static String TorchOff = "M5\r\n";
+    public static String TorchOn = "M8\r\n";
+    public static String TorchOff = "M9\r\n";
 }
