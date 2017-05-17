bfade9e77bb08d728b6bfb96d16d47e598f060da
Travis Gillin <Travis.gillin@gmail.com>
2017-05-17 17:59:39 -0400

    Adjusted x soft limits

--- src/com/nc/pilot/lib/ncConfig.java
+++ src/com/nc/pilot/lib/ncConfig.java
@@ -7,7 +7,7 @@ public class ncConfig {
     public static Integer ManualModeAcceleration = 50;
     public static Integer AutoModeAcceleration = 5;
 
-    public static Integer XMaxTravel = 50;
+    public static Integer XMaxTravel = 60;
     public static Integer XMinTravel = 0;
 
     public static Integer YMaxTravel = 82;
