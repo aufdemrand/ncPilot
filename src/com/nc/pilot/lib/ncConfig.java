96536c2ce0ce4fbb12911db37134c0b8a8f9795d
Travis Gillin <Travis.gillin@gmail.com>
2017-05-17 17:31:55 -0400

    Test

--- src/com/nc/pilot/lib/ncConfig.java
+++ src/com/nc/pilot/lib/ncConfig.java
@@ -7,11 +7,11 @@ public class ncConfig {
     public static Integer ManualModeAcceleration = 50;
     public static Integer AutoModeAcceleration = 5;
 
-    public static Integer XMaxTravel = 20;
-    public static Integer XMinTravel = 20;
+    public static Integer XMaxTravel = 100;
+    public static Integer XMinTravel = 0;
 
-    public static Integer YMaxTravel = 20;
-    public static Integer YMinTravel = 20;
+    public static Integer YMaxTravel = 100;
+    public static Integer YMinTravel = 0;
 
     public static Integer ZMaxTravel = 0;
     public static Integer ZMinTravel = -3;
